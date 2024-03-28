6c30a1010c00030b4ad6680be4a4f21986ea3f00
k0mronbek <153188218+k0mronbek@users.noreply.github.com>
2024-03-28 11:05:46 +0500

    Delete h directory

--- h/check_function.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_BC_H_CHECK_FUNTION_H_
-#define MY_BC_H_CHECK_FUNTION_H_
-short check(char c);
-short operator(char op);
-short check_minus(char* str, short ind);
-#endif
--- h/my_string.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRING_LIB
-#define STRING_LIB
-short my_isdigit(char c);
-short my_strlen(char* str);
-char* my_strcpy(char* str1, char* str2);
-void fill_null(char* str, short over);
-void free_space(char* str);
-short char_to_decimal(char* str);
-#endif
